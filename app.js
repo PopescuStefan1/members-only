b352af47f004c514ea6c9e3ac5371ba4d0ddc282
PopescuStefan1 <stefan.ro95@gmail.com>
2024-11-14 23:40:55 +0200

    Fix LocalStrategy not working with email

--- app.js
+++ app.js
@@ -3,69 +3,34 @@ import "dotenv/config";
 import path from "path";
 import session from "express-session";
 import passport from "passport";
-import LocalStrategy from "passport-local";
-import bcrypt from "bcryptjs";
 import authenticationRouter from "./routes/authenticationRouter.js";
-import db from "./db/queries.js";
 
 const app = express();
-app.set("views", path.join(import.meta.dirname, "views"));
 app.use(express.static(path.join(import.meta.dirname, "public")));
+app.set("views", path.join(import.meta.dirname, "views"));
 app.set("view engine", "ejs");
 
-passport.use(
-  new LocalStrategy(async (username, password, done) => {
-    try {
-      const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
-      const user = rows[0];
-
-      if (!user) {
-        return done(null, false, { message: "Incorrect username" });
-      }
-
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
-  })
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser(async (id, done) => {
-  try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-    const user = rows[0];
-
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
-
-const userDetailsHandler = /*async*/ (req, res, next) => {
-  console.log(req.user);
-  res.locals.user = req.user;
-  //   res.locals.userDetails = await db.getUserDetails(req.user);
-  next();
-};
-
-app.use(session({ secret: "members_only", resave: false, saveUninitialized: false }));
+app.use(session({ secret: "members-only", resave: false, saveUninitialized: false }));
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(userDetailsHandler);
+app.use((req, res, next) => {
+  console.log(req.user);
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.use("/", authenticationRouter);
 
 app.get("/", (req, res) => res.render("index"));
+app.get("/login", (req, res) => res.render("login-form"));
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
 app.get("/membership", (req, res) => res.render("membership"));
 
 app.listen(3000, () => console.log("app listening on port 3000!"));
--- routes/authenticationRouter.js
+++ routes/authenticationRouter.js
@@ -1,20 +1,55 @@
 import { Router } from "express";
 import { signUp } from "../controllers/authenticationController.js";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcryptjs";
+import pool from "../db/pool.js";
 
 const authenticationRouter = Router();
 
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, async (username, password, done) => {
+    try {
+      const { rows } = await pool.query("SELECT * FROM users WHERE email = $1", [username]);
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect email" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
 authenticationRouter.get("/");
-
 authenticationRouter.get("/sign-up", (req, res) => res.render("sign-up-form"));
-
 authenticationRouter.post("/sign-up", signUp);
 authenticationRouter.get("/login", (req, res) => res.render("login-form"));
 authenticationRouter.post(
   "/login",
-  (req, res, done) => {
-    console.log(req.body);
-    done();
-  },
   passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/",
